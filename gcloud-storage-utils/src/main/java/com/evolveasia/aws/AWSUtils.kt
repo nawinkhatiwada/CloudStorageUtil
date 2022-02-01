34b714f7a1cb8b154c6176b465a6bc47dd761ea8
Nabin Khatiwada <nawin@Nawins.local>
2022-02-01 18:21:11 +0545

    exif value set after compress

--- gcloud-storage-utils/src/main/java/com/evolveasia/aws/AWSUtils.kt
+++ gcloud-storage-utils/src/main/java/com/evolveasia/aws/AWSUtils.kt
@@ -18,78 +18,104 @@ import java.io.OutputStream
 
 fun uploadImageAWS(awsMetaInfo: AwsMetaInfo): Flowable<String> {
     return Flowable.create({ emitter ->
-      /*  val oldExif = ExifInterface(awsMetaInfo.imageMetaInfo.imagePath)
-        val compressedImagePath = compressAwsImage(awsMetaInfo).first*/
+        val oldExif = ExifInterface(awsMetaInfo.imageMetaInfo.imagePath)
+        val compressedImagePath = compressAwsImage(awsMetaInfo).first
         val compressedBitmap = compressAwsImage(awsMetaInfo).second
-           /* val newExifOrientation = setImageOrientation(oldExif, compressedImagePath)
-            if (newExifOrientation != null) {
-                try {
-                    val rotation = getRotation(newExifOrientation)
-                    if (rotation != null) {
-                        val matrix = Matrix()
-                        matrix.postRotate(rotation)
-                        setPostScale(newExifOrientation, matrix)
-                        if (compressedBitmap != null) {
-                            val rotatedBitmap = Bitmap.createBitmap(compressedBitmap, 0, 0, compressedBitmap.width, compressedBitmap.height, matrix, true)
-                            if (rotatedBitmap != null) {
-                                // rotatedBitmap will be recycled inside addAwsWaterMark function
-                                val waterMarkBitmap = addAwsWaterMark(awsMetaInfo, rotatedBitmap)
-                                waterMarkBitmap.recycle()
-                            }
-                            compressedBitmap.recycle()
-                        }
-                    } else {*/
-                        if (compressedBitmap != null) {
-                            val newBitmap = Bitmap.createBitmap(compressedBitmap, 0, 0, compressedBitmap.width, compressedBitmap.height)
-                            if (newBitmap != null) {
-                                // newBitmap will be recycled inside addAwsWaterMark function
-                                val waterMarkBitmap = addAwsWaterMark(awsMetaInfo, newBitmap)
-                                waterMarkBitmap.recycle()
-                            }
-                            compressedBitmap.recycle()
-                        }
+        setImageOrientation(oldExif, compressedImagePath)
+        /* val newExifOrientation = setImageOrientation(oldExif, compressedImagePath)
+         if (newExifOrientation != null) {
+             try {
+                 val rotation = getRotation(newExifOrientation)
+                 if (rotation != null) {
+                     val matrix = Matrix()
+                     matrix.postRotate(rotation)
+                     setPostScale(newExifOrientation, matrix)
+                     if (compressedBitmap != null) {
+                         val rotatedBitmap = Bitmap.createBitmap(compressedBitmap, 0, 0, compressedBitmap.width, compressedBitmap.height, matrix, true)
+                         if (rotatedBitmap != null) {
+                             // rotatedBitmap will be recycled inside addAwsWaterMark function
+                             val waterMarkBitmap = addAwsWaterMark(awsMetaInfo, rotatedBitmap)
+                             waterMarkBitmap.recycle()
+                         }
+                         compressedBitmap.recycle()
+                     }
+                 } else {*/
+        if (compressedBitmap != null) {
+            val newBitmap = Bitmap.createBitmap(
+                compressedBitmap,
+                0,
+                0,
+                compressedBitmap.width,
+                compressedBitmap.height
+            )
+            if (newBitmap != null) {
+                // newBitmap will be recycled inside addAwsWaterMark function
+                val waterMarkBitmap = addAwsWaterMark(awsMetaInfo, newBitmap)
+                waterMarkBitmap.recycle()
+            }
+            compressedBitmap.recycle()
+        }
 //                    }
-              /*  } catch (error: Exception) {
-                    error.printStackTrace()
-                    awsMetaInfo.imageMetaInfo.imagePath = compressedImagePath
-                }*/
+        /*  } catch (error: Exception) {
+              error.printStackTrace()
+              awsMetaInfo.imageMetaInfo.imagePath = compressedImagePath
+          }*/
 //            }
         amazonUploadSingle(awsMetaInfo)
-                ?.subscribeOn(io.reactivex.schedulers.Schedulers.io())
-                ?.observeOn(AndroidSchedulers.mainThread())
-                ?.subscribe({
-                    emitter.onComplete()
-                }) {
-                    it.printStackTrace()
-                    emitter.onError(it)
-                }
+            ?.subscribeOn(io.reactivex.schedulers.Schedulers.io())
+            ?.observeOn(AndroidSchedulers.mainThread())
+            ?.subscribe({
+                emitter.onComplete()
+            }) {
+                it.printStackTrace()
+                emitter.onError(it)
+            }
 
     }, BackpressureStrategy.LATEST)
 }
 
 //Warning amazon notifies on main thread
-fun amazonUploadSingle(awsMetaInfo: AwsMetaInfo, uploadPath: String, maxFileSize: Long): Single<TransferUpdate?>? {
-    return TransferObservable(awsMetaInfo, awsMetaInfo.imageMetaInfo.imagePath, uploadPath, maxFileSize)
-            .doOnNext { transferUpdate ->
-                if (transferUpdate.state == TransferUpdate.PROGRESSED_CHANGED)
-                    println("Data upload Progress Progress:%d , Total:%d" + transferUpdate.byteCount + transferUpdate.byteTotal)
-            }.lastOrError().observeOn(io.reactivex.schedulers.Schedulers.io())
+fun amazonUploadSingle(
+    awsMetaInfo: AwsMetaInfo,
+    uploadPath: String,
+    maxFileSize: Long
+): Single<TransferUpdate?>? {
+    return TransferObservable(
+        awsMetaInfo,
+        awsMetaInfo.imageMetaInfo.imagePath,
+        uploadPath,
+        maxFileSize
+    )
+        .doOnNext { transferUpdate ->
+            if (transferUpdate.state == TransferUpdate.PROGRESSED_CHANGED)
+                println("Data upload Progress Progress:%d , Total:%d" + transferUpdate.byteCount + transferUpdate.byteTotal)
+        }.lastOrError().observeOn(io.reactivex.schedulers.Schedulers.io())
 }
 
 fun amazonUploadSingle(awsMetaInfo: AwsMetaInfo): Single<TransferUpdate?>? {
-    return amazonUploadSingle(awsMetaInfo, awsMetaInfo.awsFolderPath, TransferObservable.FILE_SIZE_UNLIMITED)
+    return amazonUploadSingle(
+        awsMetaInfo,
+        awsMetaInfo.awsFolderPath,
+        TransferObservable.FILE_SIZE_UNLIMITED
+    )
 }
 
 fun compressAwsImage(awsMetaInfo: AwsMetaInfo): Pair<String, Bitmap?> {
 
     return try {
         val byteArray = streamToByteArray(FileInputStream(awsMetaInfo.imageMetaInfo.imagePath))
-        val bitmap = decodeSampledBitmapFromResource(byteArray, awsMetaInfo.imageMetaInfo.imageWidth
+        val bitmap = decodeSampledBitmapFromResource(
+            byteArray, awsMetaInfo.imageMetaInfo.imageWidth
                 ?: Constants.DEFAULT_IMAGE_WIDTH, awsMetaInfo.imageMetaInfo.imageHeight
-                ?: Constants.DEFAULT_IMAGE_HEIGHT, awsMetaInfo.imageMetaInfo.waterMarkInfo)
+                ?: Constants.DEFAULT_IMAGE_HEIGHT, awsMetaInfo.imageMetaInfo.waterMarkInfo
+        )
 
         val stream = ByteArrayOutputStream()
-        bitmap.compress(awsMetaInfo.imageMetaInfo.compressFormat, awsMetaInfo.imageMetaInfo.compressLevel, stream)
+        bitmap.compress(
+            awsMetaInfo.imageMetaInfo.compressFormat,
+            awsMetaInfo.imageMetaInfo.compressLevel,
+            stream
+        )
         val os: OutputStream = FileOutputStream(awsMetaInfo.imageMetaInfo.imagePath)
         os.write(stream.toByteArray())
         os.close()
@@ -100,7 +126,10 @@ fun compressAwsImage(awsMetaInfo: AwsMetaInfo): Pair<String, Bitmap?> {
     }
 }
 
-fun fetchAwsImage(serviceConfig: AwsMetaInfo.AWSConfig, queryPath: String): Single<List<AWSStorageResponse>> {
+fun fetchAwsImage(
+    serviceConfig: AwsMetaInfo.AWSConfig,
+    queryPath: String
+): Single<List<AWSStorageResponse>> {
     return Single.fromCallable {
         return@fromCallable getImageFromS3Buckets(serviceConfig, queryPath)
     }
@@ -108,18 +137,31 @@ fun fetchAwsImage(serviceConfig: AwsMetaInfo.AWSConfig, queryPath: String): Sing
 }
 
 
-fun getImageFromS3Buckets(serviceConfig: AwsMetaInfo.AWSConfig, queryPath: String): List<AWSStorageResponse> {
+fun getImageFromS3Buckets(
+    serviceConfig: AwsMetaInfo.AWSConfig,
+    queryPath: String
+): List<AWSStorageResponse> {
     // Queries files in the bucket from S3.
     val s3 = Util.getS3Client(serviceConfig)
     var objectList = s3.listObjects(serviceConfig.bucketName, queryPath)
     val list = objectList.objectSummaries.map {
-        AWSStorageResponse(s3.getUrl(serviceConfig.bucketName, it.key).toString(), it.lastModified, it.owner.displayName, it.size)
+        AWSStorageResponse(
+            s3.getUrl(serviceConfig.bucketName, it.key).toString(),
+            it.lastModified,
+            it.owner.displayName,
+            it.size
+        )
     }.toMutableList()
 
     while (!objectList.isTruncated) {
         objectList = getObjectList(objectList, s3)
         list.addAll(objectList.objectSummaries.map {
-            AWSStorageResponse(s3.getUrl(serviceConfig.bucketName, it.key).toString(), it.lastModified, it.owner.displayName, it.size)
+            AWSStorageResponse(
+                s3.getUrl(serviceConfig.bucketName, it.key).toString(),
+                it.lastModified,
+                it.owner.displayName,
+                it.size
+            )
         })
     }
     return list
@@ -131,8 +173,9 @@ fun getObjectList(objectListing: ObjectListing, s3: AmazonS3Client): ObjectListi
 
 private fun getImageOrientation(exif: ExifInterface): Int {
     return exif.getAttributeInt(
-            ExifInterface.TAG_ORIENTATION,
-            ExifInterface.ORIENTATION_NORMAL)
+        ExifInterface.TAG_ORIENTATION,
+        ExifInterface.ORIENTATION_NORMAL
+    )
 }
 
 private fun setImageOrientation(oldExif: ExifInterface, newImagePath: String): Int? {
@@ -141,7 +184,10 @@ private fun setImageOrientation(oldExif: ExifInterface, newImagePath: String): I
         val newExif = ExifInterface(newImagePath)
         newExif.setAttribute(ExifInterface.TAG_ORIENTATION, exifOrientation)
         newExif.saveAttributes()
-        return newExif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED)
+        return newExif.getAttributeInt(
+            ExifInterface.TAG_ORIENTATION,
+            ExifInterface.ORIENTATION_UNDEFINED
+        )
     }
     return null
 }
